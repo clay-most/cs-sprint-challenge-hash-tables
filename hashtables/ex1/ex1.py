1aec9efec87d2a0c37e544ee7af8afac17bd869f
Anna Morris <annapatton444@gmail.com>
2020-10-11 18:45:15 -0500

    example 1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -2,6 +2,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    store = {}
+
+    for i in range(length):
+
+        weight = weights[i]
+        x = limit - weight
+
+        if x not in store:
+            store[weight] = i
+        elif x in store:
+            if i > store[x]:
+                return (i, store[x])
+            else:
+                return (store[x], i)
 
     return None
